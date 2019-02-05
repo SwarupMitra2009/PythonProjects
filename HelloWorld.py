79b53143050ab3d1235193f71988bab67d8b1639
SwarupMitra2009 <47347279+SwarupMitra2009@users.noreply.github.com>
2019-02-05 19:29:58 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+print("Hello World")
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
